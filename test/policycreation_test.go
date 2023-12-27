3c3fb64babf0e4692b12ea7a86df1d6b14237665
Jiahui Feng <jhf@google.com>
2023-12-27 13:45:28 -0800

    finish up creation test.

--- test/policycreation_test.go
+++ test/policycreation_test.go
@@ -11,38 +11,38 @@ import (
 )
 
 func TestConcurrentPolicyCreation(t *testing.T) {
-	var C = 8
-	var D = time.Minute
-	ctx := context.Background()
-	policy, err := loadYAMLTestData[admissionregistrationv1beta1.ValidatingAdmissionPolicy]("image-matches-namespace-environment.policy.yaml")
-	if err != nil {
-		t.Fatalf("fail to load test data: %v", err)
-	}
-	client := mustTestClient(t)
-	b := &benchmark{
-		Concurrency: C,
-		Duration:    D,
-		Name:        "creation",
-		Func: func(ctx context.Context, b *benchmark) error {
-			policy := policy.DeepCopy()
-			policy.Name = b.RandomResourceName()
-			_, err := client.AdmissionregistrationV1beta1().ValidatingAdmissionPolicies().Create(ctx, policy, metav1.CreateOptions{})
-			// ignore timeout for benchmark
-			if errors.Is(err, context.DeadlineExceeded) {
-				return nil
-			}
-			return err
-		},
-	}
-	r, err := b.Run(context.Background())
-	if err != nil {
-		t.Fatalf("fail to create: %v", err)
-	}
-	t.Logf("QPS: %f", r.qps)
-	err = client.AdmissionregistrationV1beta1().ValidatingAdmissionPolicies().DeleteCollection(ctx, metav1.DeleteOptions{}, metav1.ListOptions{
-		LabelSelector: b.LabelSelector(),
-	})
-	if err != nil {
-		t.Fatalf("fail to cleanup: %v", err)
+	for C := 1; C <= 16; C *= 2 {
+		ctx := context.Background()
+		policy, err := loadYAMLTestData[admissionregistrationv1beta1.ValidatingAdmissionPolicy]("image-matches-namespace-environment.policy.yaml")
+		if err != nil {
+			t.Fatalf("fail to load test data: %v", err)
+		}
+		client := mustTestClient(t)
+		b := &benchmark{
+			Concurrency: C,
+			Duration:    time.Minute * 2,
+			Name:        "creation",
+			Func: func(ctx context.Context, b *benchmark) error {
+				policy := policy.DeepCopy()
+				policy.Name = b.RandomResourceName()
+				_, err := client.AdmissionregistrationV1beta1().ValidatingAdmissionPolicies().Create(ctx, policy, metav1.CreateOptions{})
+				// ignore timeout for benchmark
+				if errors.Is(err, context.DeadlineExceeded) {
+					return nil
+				}
+				return err
+			},
+		}
+		r, err := b.Run(context.Background())
+		if err != nil {
+			t.Fatalf("fail to create: %v", err)
+		}
+		t.Logf("Currency: %2d, QPS: %f", C, r.qps)
+		err = client.AdmissionregistrationV1beta1().ValidatingAdmissionPolicies().DeleteCollection(ctx, metav1.DeleteOptions{}, metav1.ListOptions{
+			LabelSelector: b.LabelSelector(),
+		})
+		if err != nil {
+			t.Fatalf("fail to cleanup: %v", err)
+		}
 	}
 }
